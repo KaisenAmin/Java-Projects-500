8e62322b44a47cb7a77593eff6f5a757cc91e0e7
amin tahmasebi <amintahmasebi479@gmail.com>
2025-12-08 15:01:52 +0330

    11 - check number is pos or neg or zero

---
+++ 11 - number is pos or neg or zero/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Enter number please : ");
+        number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println(number + " is positive");
+        } else if (number < 0) {
+            System.out.println(number + " is negative");
+        } else {
+            System.out.println(number + " is zero");
+        }
+
+    }
+}
